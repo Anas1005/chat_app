46b15b97d338bb074265d90a9d0e5f5416628538
Anas <anassaif.507@gmail.com>
2024-01-02 20:58:54 +0530

    updatedRouter

--- MessengerBackEnd/routes/index.js
+++ MessengerBackEnd/routes/index.js
@@ -6,4 +6,11 @@ const userRoute = require("./userRoute");
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
 
+router.get("/getDetails",(req,res)=>{
+    return res.status(200).json({
+        success: true,
+        message: "Connected..."
+    });
+})
+
 module.exports = router;
