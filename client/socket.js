3574acfef3634600c30dcc6729ca797425ea4ca0
Anas <anassaif.507@gmail.com>
2024-01-02 22:16:06 +0530

    updated Socket

--- client/socket.js
+++ client/socket.js
@@ -1,5 +1,8 @@
 import io from 'socket.io-client';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const socketServerURL = isProduction ? 'https://chat-app-backend-f9jz.onrender.com' : 'http://localhost:4000';
+
 let socket=null;
 
 const connectSocket = (user_id) => {
@@ -7,7 +10,7 @@ const connectSocket = (user_id) => {
   let newSocket;
     
   return new Promise((resolve, reject) => {
-    newSocket = io('http://localhost:4000/', {
+    newSocket = io(socketServerURL, {
       query: `user_id=${user_id}`,
     });
 
